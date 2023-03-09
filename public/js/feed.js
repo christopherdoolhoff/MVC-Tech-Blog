772126a68fda0a44cc6deef32a9d9c3af711f8c1
CDoolhoff <109476450+christopherdoolhoff@users.noreply.github.com>
2023-03-08 23:03:21 -0500

    corrected problem with feed.

--- public/js/feed.js
+++ public/js/feed.js
@@ -16,14 +16,7 @@ window.onclick = function(event) {
     }
 };
 
-// const test = async (event) => {
-//     event.preventDefault();
-//     const session = await fetch ('api/users/session');
-//     console.log('test session', session)
-//     alert('test');
-// }
 const feed = async () => {
-    // event.preventDefault();
         const feedData = await fetch ('/api/users/posts');
         const feedInfo = await feedData.json();
         console.log(feedInfo);
@@ -32,9 +25,6 @@ const feed = async () => {
   
 feed();
 
-//     document
-//     .querySelector('#testBtn')
-//     .addEventListener('click', test);
 var feedBoxEl = document.querySelector(".feed");
 var feedResults = function (data) {
     console.log(data);
